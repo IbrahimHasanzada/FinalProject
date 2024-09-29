b9b0335bfa813de0c6d03832e4765da46285c667
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2024-09-29 14:29:01 +0400

    fix problems

--- src/Components/User/Header/Header.jsx
+++ src/Components/User/Header/Header.jsx
@@ -72,7 +72,7 @@ const Header = () => {
                 </div>
                 <div className="w-[130px] h-[30px] lg:w-[155px] lg:h-[30px]">
                     <Link to="/">
-                        <img src="/img/tradium (1).png" alt="emporiumLogo" />
+                        <img src="/img/tradium.png" alt="emporiumLogo" />
                     </Link>
                 </div>
                 <div className=" flex gap-4 lg:w-[45%] justify-end">
