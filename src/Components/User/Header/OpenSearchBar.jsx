fee03a9c659b7f0106d096c662e5269ad06484c7
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2024-10-05 08:50:59 +0400

    fixed bugs

--- src/Components/User/Header/OpenSearchBar.jsx
+++ src/Components/User/Header/OpenSearchBar.jsx
@@ -40,14 +40,14 @@ const OpenSearchBar = ({ flag, setFlag }) => {
                 <div className="flex flex-col">
                     <div className="flex items-center justify-center gap-4">
                         <form className=" h-11 lg:w-full w-[90%] pl-5 pr-2 flex items-center justify-between border my-5">
-                            <input onChange={(e) => setSearch(e.target.value)} type="text" className="outline-none w-full" placeholder="Axtar..." />
+                            <input onChange={(e) => setSearch(e.target.value)} type="text" className="outline-none w-full" placeholder="Search..." />
                             <IoIosSearch className="text-2xl cursor-pointer" />
                         </form>
                         <FaXmark className="cursor-pointer text-xl block lg:hidden " onClick={handleCloseSearch} />
                     </div>
                     <div className="max-w-[700px]">
                         <ul className='flex py-2 lg:px-5 w-full overflow-scroll'>
-                            <li onClick={() => setCatName('')} className={`py-1 px-2 ml-[2px] mb-1 cursor-pointer rounded-[3px]  hover:bg-[#e4e4e4] whitespace-nowrap ${!catName && 'bg-[#e4e4e4]'}`}>Hamısı</li>
+                            <li onClick={() => setCatName('')} className={`py-1 px-2 ml-[2px] mb-1 cursor-pointer rounded-[3px]  hover:bg-[#e4e4e4] whitespace-nowrap ${!catName && 'bg-[#e4e4e4]'}`}>All</li>
                             {categories?.map((item, i) => (
                                 <li onClick={() => setCatName(item.name)} key={i} className={`py-1 px-2 ml-[2px] mb-1 cursor-pointer rounded-[3px] hover:bg-[#e4e4e4] ${catName === item.name && 'bg-[#e4e4e4]'} whitespace-nowrap`}>{item.name}</li>
                             ))}
